6566b9e54e64cecee95b82d228c443842185e6cf
Sidharth Shankar <sidharth.shankar@mountblue.tech>
2020-04-06 12:10:13 +0530

    Everything got completed for this project, except the working progress bar

--- domAnimation.js
+++ domAnimation.js
@@ -17,7 +17,10 @@ document.addEventListener("DOMContentLoaded", function(){
         return response.json();
      })
      .then((data) => {
-           fetchAllCards(data);
+        fetchAllCards(data);
+     })
+     .catch(error => {
+        window.location.href = "./Error.html";
      });
 });
 
@@ -48,22 +51,23 @@ function fetchAllCards(data) {
 
 function fetchAllChecklist(cardId) {
   let url = `${base_url}cards/${cardId}/checklists?key=${APIkey}&token=${token}`;
+  console.log(url);
   fetch(url)
      .then((response) => {
         return response.json();
      })
      .then((data) => {
-           fetchAllChecklistData(data);
+        fetchAllChecklistData(data);
      });
 
 }
 
 function fetchAllChecklistData(data){
   let modalbody = document.getElementById('modalBody');
-  data.forEach(checkistObj => {
+  data.forEach(checklistObj => {
     let pElement = document.createElement('p');
     pElement.className = 'checklist-title';
-    pElement.appendChild(document.createTextNode(checkistObj.name));
+    pElement.appendChild(document.createTextNode(checklistObj.name));
 
     let deleteChecklist = document.createElement('button');
     deleteChecklist.className = 'button delete-checklist';
@@ -81,6 +85,7 @@ function fetchAllChecklistData(data){
     progressbar.setAttribute('aria-valuenow', '0');
     progressbar.setAttribute('aria-valuemin', 0);
     progressbar.setAttribute('aria-valuemax', 100);
+    progressbar.setAttribute('data-checkItemCount', 0);
     progressbar.appendChild(document.createTextNode('0%'));
 
     let progressDiv = document.createElement('div');
@@ -116,16 +121,71 @@ function fetchAllChecklistData(data){
     let addItemBtn = document.createElement('button');
     addItemBtn.className = 'button add-items';
     addItemBtn.appendChild(document.createTextNode('Add item'));
+    
+    if(checklistObj.checkItems.length > 0) {
+      checklistObj.checkItems.forEach(checkitemObj => {
+        let checkboxTitle = checkitemObj.name;
+        console.log(checkboxTitle);
+        let checkBoxInput = document.createElement('input');
+        checkBoxInput.className = 'form-check-input';
+        checkBoxInput.type = 'checkbox';
+        checkBoxInput.value = "";
 
+        let checkboxLabel = document.createElement('label');
+        checkboxLabel.className = 'form-check-label';
+        checkboxLabel.setAttribute('data-checkitemid', checkitemObj.id);
+        checkboxLabel.appendChild(document.createTextNode(checkboxTitle))
+        if(checkitemObj.state === 'complete') {
+          checkBoxInput.checked = true;
+          let delElement = document.createElement('del');
+          delElement.appendChild(document.createTextNode(checkboxLabel.textContent));
+          checkboxLabel.textContent = "";
+          checkboxLabel.appendChild(delElement);
+        }
+
+        let updateItemInput = document.createElement('input');
+        updateItemInput.className = 'form-control checkItem';
+        updateItemInput.type = 'text';
+        updateItemInput.value = "";
+
+        let updateBtn = document.createElement('button');
+        updateBtn.className = 'btn btn-success update-checkItem';
+        updateBtn.type = 'button';
+        updateBtn.appendChild(document.createTextNode('update'));
+
+        let deleteItemBtn = document.createElement('button');
+        deleteItemBtn.className = 'btn btn-secondary delete-checkItem';
+        deleteItemBtn.type = 'button';
+        deleteItemBtn.appendChild(document.createTextNode('Delete Item'));
+
+        let cancelUpdate = document.createElement('button');
+        cancelUpdate.className = 'btn btn-danger cancel-checkItem';
+        cancelUpdate.type = 'button';
+        cancelUpdate.appendChild(document.createTextNode('cancel'));
+
+        let updateElementDiv = document.createElement('div');
+        updateElementDiv.className = 'update-item-area';
+        updateElementDiv.appendChild(updateItemInput);
+        updateElementDiv.appendChild(updateBtn);
+        updateElementDiv.appendChild(deleteItemBtn);
+        updateElementDiv.appendChild(cancelUpdate);
+
+        let formCheckElement = document.createElement('div');
+        formCheckElement.className = 'form-check';
+        formCheckElement.appendChild(checkBoxInput);
+        formCheckElement.appendChild(checkboxLabel);
+        formCheckElement.appendChild(updateElementDiv);
+        checklistContent.appendChild(formCheckElement);
+      })
+      
+    }
     let checklistContainer = document.createElement('div');
     checklistContainer.className = 'checklist-container';
     checklistContainer.appendChild(checklistContent);
     checklistContainer.appendChild(addItemTextArea);
     checklistContainer.appendChild(addItemBtn);
-
-    checklistContainer.setAttribute('data-checklistId', checkistObj.id);
+    checklistContainer.setAttribute('data-checklistId', checklistObj.id);
     modalbody.appendChild(checklistContainer);
-
   }) 
 }
 
@@ -152,6 +212,18 @@ lists.onclick = function(event) {
     addItemsInChecklist(event);
   } else if(event.target.className === 'btn btn-danger cancel-item') {
     hideAdditemTextArea(event);
+  } else if(event.target.className === 'form-check-input') {
+    markItemAsChecked(event);
+  } else if(event.target.className === 'button delete-checklist') {
+    deleteChecklist(event);
+  } else if(event.target.className === 'form-check-label') {
+    showCheckListItemMenu(event);
+  } else if(event.target.className === 'btn btn-success update-checkItem') {
+    updateCheckItem(event);
+  } else if(event.target.className === 'btn btn-secondary delete-checkItem') {
+    deleteCheckItem(event);
+  } else if(event.target.className === 'btn btn-danger cancel-checkItem') {
+    cancelCheckItemUpdate(event);
   }
    else {
     return;
@@ -180,6 +252,46 @@ function deleteCards(event) {
   });
 }
 
+function deleteChecklist(e) {
+  let checklistContainer = e.target.closest('.checklist-container');
+  let url = `${base_url}cards/${cardIdOnModal}/checklists/${checklistContainer.dataset.checklistid}?key=${APIkey}&token=${token}`;
+  fetch(url, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  })
+  .then((response) => response.json())
+  .then((data) => {
+    checklistContainer.remove();
+    console.log('Successfully removed');
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
+  console.log(url);
+}
+function deleteCheckItem(e) {
+  let checklistId = e.target.closest('.checklist-container').dataset.checklistid;
+  let checkItemId = e.target.parentElement.previousSibling.dataset.checkitemid;
+  let url = `${base_url}checklists/${checklistId}/checkItems/${checkItemId}?key=${APIkey}&token=${token}`;
+  fetch(url, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  })
+  .then((response) => response.json())
+  .then((data) => {
+    e.target.parentElement.parentElement.remove();
+    console.log('Successfully removed');
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
+  console.log(url);
+}
+
 function showTextArea() {
     document.getElementById('addAnotherCard').style.display = 'none';
     document.getElementById('titleSpace').style.display = 'block';
@@ -200,8 +312,8 @@ function hideCheckListTextArea() {
 function displayCardNameOnModal(e) {
   document.getElementById('modalTitle').textContent = e.target.textContent;
   cardIdOnModal = e.target.dataset.cardid;
-  fetchAllChecklist(e.target.dataset.cardid)
-
+  fetchAllChecklist(e.target.dataset.cardid);  //check here using flag that this method
+  //fetchAllchecklistItems(e.target.dataset.);    //should not get called 2nd time.
 }
 function showAddItemTextArea(e) {
   e.target.style.display = 'none';
@@ -211,26 +323,155 @@ function hideAdditemTextArea(e) {
   e.target.parentElement.nextSibling.style.display = 'block';
   e.target.parentElement.style.display = 'none';
 }
+function showCheckListItemMenu(e) {
+  Array.from(document.getElementsByClassName('update-item-area')).forEach(classEle => {
+    if(classEle.style.display !== 'none') {
+      classEle.style.display = 'none';
+    }
+  });
+  e.target.nextSibling.style.display = 'block';
+  e.target.closest('.checklist-container').lastElementChild.style.display = 'none';
+}
+function cancelCheckItemUpdate(e) {
+  e.target.parentElement.style.display = 'none';
+  e.target.closest('.checklist-container').lastElementChild.style.display = 'block';
+}
+
+function markItemAsChecked(e) {
+  // console.log(e.target.checked);
+  let checkboxLabel = e.target.nextSibling;
+  let progressElement = e.target.parentElement.previousSibling.firstElementChild;
+  let checkItemCount = progressElement.dataset.checkitemcount;
+  
+  if(e.target.checked) {
+    let url =`${base_url}cards/${cardIdOnModal}/checkItem/${checkboxLabel.dataset.checkitemid}?state=complete&key=${APIkey}&token=${token}`; 
+    console.log(url);
+    fetch(url, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    })
+    .then((response) => response.json())
+    .then((data) => {
+      let delElement = document.createElement('del');
+      delElement.appendChild(document.createTextNode(checkboxLabel.textContent));
+      checkboxLabel.textContent = "";
+      checkboxLabel.appendChild(delElement);
+      console.log('Success:', data.state);
+    })
+    .catch((error) => {
+      // window.location.href = "./Error.html";
+      console.error('Error:', error);
+    });
+    // progressElement.dataset.checkitemcount
+    console.log(e.target.nextSibling);
+  } else {
+    let url =`${base_url}cards/${cardIdOnModal}/checkItem/${checkboxLabel.dataset.checkitemid}?state=incomplete&key=${APIkey}&token=${token}`;
+    fetch(url, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    })
+    .then((response) => response.json())
+    .then((data) => {
+      let text = checkboxLabel.firstElementChild.textContent;
+      checkboxLabel.firstElementChild.remove();
+      checkboxLabel.textContent = text;
+      console.log('Success:', data.state);
+    })
+    .catch((error) => {
+      // window.location.href = "./Error.html";
+      console.error('Error:', error);
+    });
+  }
+}
+function updateCheckItem(e) {
+  let checkItemLabel = e.target.closest('.update-item-area').previousSibling;
+  console.log(checkItemLabel);
+  let url =`${base_url}cards/${cardIdOnModal}/checkItem/${checkItemLabel.dataset.checkitemid}?name=${e.target.previousSibling.value}&key=${APIkey}&token=${token}`; 
+  fetch(url, {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  })
+  .then((response) => response.json())
+  .then((data) => {
+    checkItemLabel.textContent = e.target.previousSibling.value;
+    console.log('Success:', data.name);
+  })
+  .catch((error) => {
+    // window.location.href = "./Error.html";
+    console.error('Error:', error);
+  });
+
+}
+
 function addItemsInChecklist(event) {
-  let checklistItem = event.target.closest('.checklist-container');
-  let checkListContent = checklistItem.firstElementChild;
+  let checklistContainer = event.target.closest('.checklist-container');
+  let checkListContent = checklistContainer.firstElementChild;
   let checkboxTitle = event.target.previousSibling.value;
-  console.log(checkboxTitle);
-  let checkBoxInput = document.createElement('input');
-  checkBoxInput.className = 'form-check-input';
-  checkBoxInput.type = 'checkbox';
-  checkBoxInput.value = "";
+  // console.log(checkboxTitle);
+  let url = `${base_url}checklists/${checklistContainer.dataset.checklistid}/checkItems?name=${checkboxTitle}&key=${APIkey}&token=${token}`;
+  console.log(url);
+  fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    }
+  })
+  .then((response) => response.json())
+  .then((data) => {
+    let checkBoxInput = document.createElement('input');
+    checkBoxInput.className = 'form-check-input';
+    checkBoxInput.type = 'checkbox';
+    checkBoxInput.value = "";
 
-  let checkboxLabel = document.createElement('label');
-  checkboxLabel.className = 'form-check-label';
-  checkboxLabel.appendChild(document.createTextNode(checkboxTitle))
+    let checkboxLabel = document.createElement('label');
+    checkboxLabel.className = 'form-check-label';
+    checkboxLabel.setAttribute('data-checkitemid', data.id)
+    checkboxLabel.appendChild(document.createTextNode(checkboxTitle))
 
-  let formCheckElement = document.createElement('div');
-  formCheckElement.className = 'form-check';
-  formCheckElement.appendChild(checkBoxInput);
-  formCheckElement.appendChild(checkboxLabel);
+    let updateItemInput = document.createElement('input');
+    updateItemInput.className = 'form-control checkItem';
+    updateItemInput.type = 'text';
+    updateItemInput.value = "";
 
-  checkListContent.appendChild(formCheckElement);
+    let updateBtn = document.createElement('button');
+    updateBtn.className = 'btn btn-success update-checkItem';
+    updateBtn.type = 'button';
+    updateBtn.appendChild(document.createTextNode('update'));
+
+    let deleteItemBtn = document.createElement('button');
+    deleteItemBtn.className = 'btn btn-secondary delete-checkItem';
+    deleteItemBtn.type = 'button';
+    deleteItemBtn.appendChild(document.createTextNode('Delete Item'));
+
+    let cancelUpdate = document.createElement('button');
+    cancelUpdate.className = 'btn btn-danger cancel-checkItem';
+    cancelUpdate.type = 'button';
+    cancelUpdate.appendChild(document.createTextNode('cancel'));
+
+    let updateElementDiv = document.createElement('div');
+    updateElementDiv.className = 'update-item-area';
+    updateElementDiv.appendChild(updateItemInput);
+    updateElementDiv.appendChild(updateBtn);
+    updateElementDiv.appendChild(deleteItemBtn);
+    updateElementDiv.appendChild(cancelUpdate);
+
+    let formCheckElement = document.createElement('div');
+    formCheckElement.className = 'form-check';
+    formCheckElement.appendChild(checkBoxInput);
+    formCheckElement.appendChild(checkboxLabel);
+    formCheckElement.appendChild(updateElementDiv);
+    checkListContent.appendChild(formCheckElement);
+    console.log('Success:', data.name);
+  })
+  .catch((error) => {
+    console.error('Error:', error);
+  });
   console.log(checkListContent);
 }
 
@@ -256,6 +497,7 @@ function addCheckListToTheCard() {
   progressbar.setAttribute('aria-valuenow', '0');
   progressbar.setAttribute('aria-valuemin', 0);
   progressbar.setAttribute('aria-valuemax', 100);
+  progressbar.setAttribute('data-checkItemCount', 0);
   progressbar.appendChild(document.createTextNode('0%'));
 
   let progressDiv = document.createElement('div');
